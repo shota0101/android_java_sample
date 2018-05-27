ff7207fe8c34e2896d3b16d490f6aaffdac7fa19
Shota Hayashi <qsezy1@hotmail.com>
2018-05-27 17:52:24 +0900

    ボタンを追加

--- app/src/main/java/com/hayashi/button_sample/MainActivity.java
+++ app/src/main/java/com/hayashi/button_sample/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button = null;
+    private final String TAG = "----";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        button = (Button) findViewById(R.id.button_test);
+        // ボタンがクリックされた時に呼び出されるコールバックリスナーを登録します
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "onClick()",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
